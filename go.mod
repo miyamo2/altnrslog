7cdb57d87eb49809e9fc4eba8bbe02506b658950
miyamo2 <miyamo2@outlook.com>
2024-02-27 02:44:50 +0900

    add: transactional handler

---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/miyamo2/altnrslog
+
+go 1.21
+
+require (
+	github.com/google/go-cmp v0.6.0
+	github.com/newrelic/go-agent/v3 v3.30.0
+	github.com/newrelic/go-agent/v3/integrations/logcontext-v2/logWriter v1.0.1
+	go.uber.org/mock v0.4.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/newrelic/go-agent/v3/integrations/logcontext-v2/nrwriter v1.0.0 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
+	google.golang.org/grpc v1.56.3 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
---
+++ transactional_handler_test.go
@@ -0,0 +1,255 @@
+package altnrslog
+
+import (
+	"context"
+	"errors"
+	"github.com/google/go-cmp/cmp"
+	mslog "github.com/miyamo2/altnrslog/mock"
+	"github.com/newrelic/go-agent/v3/integrations/logcontext"
+	"github.com/newrelic/go-agent/v3/newrelic"
+	"go.uber.org/mock/gomock"
+	"log/slog"
+	"testing"
+)
+
+type mockWriter struct{}
+
+func (m mockWriter) Write(p []byte) (n int, err error) {
+	return 0, nil
+}
+
+func Test_attrsFromMetadata(t *testing.T) {
+	type test struct {
+		args newrelic.LinkingMetadata
+		want []slog.Attr
+	}
+
+	tests := map[string]test{
+		"happy-path: valid linking metadata": {
+			args: newrelic.LinkingMetadata{
+				TraceID:    "trace-id",
+				SpanID:     "span-id",
+				EntityName: "entity-name",
+				EntityType: "entity-type",
+				EntityGUID: "entity-guid",
+				Hostname:   "hostname",
+			},
+			want: []slog.Attr{
+				slog.String(logcontext.KeyTraceID, "trace-id"),
+				slog.String(logcontext.KeySpanID, "span-id"),
+				slog.String(logcontext.KeyEntityName, "entity-name"),
+				slog.String(logcontext.KeyEntityType, "entity-type"),
+				slog.String(logcontext.KeyEntityGUID, "entity-guid"),
+				slog.String(logcontext.KeyHostname, "hostname"),
+			},
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := attrsFromMetadata(tt.args)
+			if diff := cmp.Diff(got, tt.want); diff != "" {
+				t.Error(diff)
+			}
+		})
+	}
+}
+
+func Test_NewTransactionalHandler_WithJSONHandler(t *testing.T) {
+	app := newrelic.Application{}
+	tx := newrelic.Transaction{}
+	options := []HandlerOption{WithSlogHandlerSpecify(true, nil)}
+	handler := NewTransactionalHandler(&app, &tx, options...)
+	_, ok := handler.handler.(*slog.JSONHandler)
+	if !ok {
+		t.Errorf("NewTransactionalHandler() = %T, want %T", handler.handler, &slog.JSONHandler{})
+	}
+}
+
+func Test_NewTransactionalHandler_WithTextHandler(t *testing.T) {
+	app := newrelic.Application{}
+	tx := newrelic.Transaction{}
+	options := []HandlerOption{WithSlogHandlerSpecify(false, nil)}
+	handler := NewTransactionalHandler(&app, &tx, options...)
+	_, ok := handler.handler.(*slog.TextHandler)
+	if !ok {
+		t.Errorf("NewTransactionalHandler() = %T, want %T", handler.handler, &slog.TextHandler{})
+	}
+}
+
+func TestTransactionalHandler_WithAttrs(t *testing.T) {
+	app := newrelic.Application{}
+	tx := newrelic.Transaction{}
+	sut := NewTransactionalHandler(&app, &tx, WithSlogHandlerSpecify(true, nil))
+	handler := sut.WithAttrs([]slog.Attr{})
+	outerHandler, ok := handler.(*TransactionalHandler)
+	if !ok {
+		t.Errorf("WithAttrs() = %T, want %T", handler, &TransactionalHandler{})
+	}
+	_, ok = outerHandler.handler.(*slog.JSONHandler)
+	if !ok {
+		t.Errorf("WithAttrs() inner = %T, want %T", outerHandler.handler, &slog.JSONHandler{})
+	}
+}
+
+func TestTransactionalHandler_WithGroup(t *testing.T) {
+	app := newrelic.Application{}
+	tx := newrelic.Transaction{}
+	sut := NewTransactionalHandler(&app, &tx, WithSlogHandlerSpecify(true, nil))
+	handler := sut.WithGroup("foo")
+	outerHandler, ok := handler.(*TransactionalHandler)
+	if !ok {
+		t.Errorf("WithGroup() = %T, want %T", handler, &TransactionalHandler{})
+	}
+	_, ok = outerHandler.handler.(*slog.JSONHandler)
+	if !ok {
+		t.Errorf("WithGroup() inner = %T, want %T", outerHandler.handler, &slog.JSONHandler{})
+	}
+}
+
+func TestTransactionalHandler_Enabled(t *testing.T) {
+	type fields struct {
+		handler slog.Handler
+	}
+	type args struct {
+		ctx   context.Context
+		level slog.Level
+	}
+	type test struct {
+		fields fields
+		args   args
+		want   bool
+	}
+	tests := map[string]test{
+		"happy-path enable": {
+			fields: fields{
+				handler: slog.NewTextHandler(&mockWriter{}, &slog.HandlerOptions{
+					Level: slog.LevelWarn,
+				}),
+			},
+			args: args{
+				ctx:   context.Background(),
+				level: slog.LevelWarn,
+			},
+			want: true,
+		},
+		"happy-path disable": {
+			fields: fields{
+				handler: slog.NewTextHandler(&mockWriter{}, &slog.HandlerOptions{
+					Level: slog.LevelWarn,
+				}),
+			},
+			args: args{
+				ctx:   context.Background(),
+				level: slog.LevelInfo,
+			},
+			want: false,
+		},
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			h := &TransactionalHandler{
+				handler: tt.fields.handler,
+			}
+			if got := h.Enabled(tt.args.ctx, tt.args.level); got != tt.want {
+				t.Errorf("Enabled() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_buildProperties(t *testing.T) {
+	type args struct {
+		options []HandlerOption
+	}
+	type test struct {
+		args args
+		want *Properties
+	}
+	tests := map[string]test{
+		"happy-path: WithInnerWriter": {
+			args: args{
+				options: []HandlerOption{WithInnerWriter(&mockWriter{})},
+			},
+			want: &Properties{
+				innerWriter: &mockWriter{},
+			},
+		},
+		"happy-path: WithSlogHandlerSpecify": {
+			args: args{
+				options: []HandlerOption{WithSlogHandlerSpecify(true, &slog.HandlerOptions{
+					AddSource: true,
+					Level:     slog.LevelDebug,
+				})},
+			},
+			want: &Properties{
+				json: true,
+				slogHandlerOptions: &slog.HandlerOptions{
+					AddSource: true,
+					Level:     slog.LevelDebug,
+				},
+			},
+		},
+	}
+	opt := cmp.AllowUnexported(Properties{})
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := buildProperties(tt.args.options)
+			if diff := cmp.Diff(*got, *tt.want, opt); diff != "" {
+				t.Error(diff)
+			}
+		})
+	}
+}
+
+func TestTransactionalHandler_Handle(t *testing.T) {
+
+	type args struct {
+		r slog.Record
+	}
+
+	type mockExpect struct {
+		args args
+		err  error
+	}
+
+	type test struct {
+		args       args
+		mockExpect mockExpect
+		want       error
+	}
+	tests := map[string]test{
+		"happy-path": {
+			args: args{
+				r: slog.Record{},
+			},
+			mockExpect: mockExpect{
+				err: nil,
+			},
+			want: nil,
+		},
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			mockCtrl := gomock.NewController(t)
+			defer mockCtrl.Finish()
+			mockHandler := mslog.NewMockHandler(mockCtrl)
+			mockHandler.EXPECT().Handle(gomock.Any(), testHelper_DummySlogRecord(t)).Return(tt.mockExpect.err).Times(1)
+			h := &TransactionalHandler{
+				handler: mockHandler,
+				tx:      &newrelic.Transaction{},
+			}
+			err := h.Handle(context.Background(), tt.args.r)
+			if !errors.Is(err, tt.want) {
+				t.Errorf("Handle() = %v, want %v", err, tt.want)
+			}
+		})
+	}
+}
+
+func testHelper_DummySlogRecord(t *testing.T) slog.Record {
+	t.Helper()
+	r := slog.Record{}
+	r.AddAttrs(attrsFromMetadata(newrelic.LinkingMetadata{})...)
+	return r
+}
