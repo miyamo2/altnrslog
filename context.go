44754f8877ecf55853bbd7f9f936ecbaf3e149a1
miyamo2 <79917704+miyamo2@users.noreply.github.com>
2024-02-27 08:37:58 +0900

    Feature: #14 Mod godoc hyperlink (#15)

--- context.go
+++ context.go
@@ -15,8 +15,8 @@ var (
 	ErrNotStored = errors.New("logger not stored")
 )
 
-// FromContext returns *slog.Logger with *TransactionalHandler, stored in the context.Context.
-// If it does not exist, return ErrNotStored.
+// FromContext returns [*slog.Logger] with [*TransactionalHandler], stored in the context.Context.
+// If it does not exist, return [ErrNotStored].
 func FromContext(ctx context.Context) (*slog.Logger, error) {
 	logger, ok := ctx.Value(loggerKey{}).(*slog.Logger)
 	if !ok {
@@ -29,8 +29,8 @@ func FromContext(ctx context.Context) (*slog.Logger, error) {
 	return logger, nil
 }
 
-// StoreToContext stores the *slog.Logger in context.Context.
-// Logger must be set to *TransactionalHandler in the Handler
+// StoreToContext stores the [*slog.Logger] in [context.Context].
+// Logger must be set to [*TransactionalHandler] in the Handler
 func StoreToContext(ctx context.Context, logger *slog.Logger) (context.Context, error) {
 	_, ok := logger.Handler().(*TransactionalHandler)
 	if !ok {
