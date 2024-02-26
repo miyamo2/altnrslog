244b17517d75da1f7cba229389428f8c3047e2f5
miyamo2 <miyamo2@outlook.com>
2024-02-27 08:27:52 +0900

    add: document overview

---
+++ doc.go
@@ -0,0 +1,6 @@
+// altnrslog is an alternative library for [Logs in Context] with [log.slog].
+//
+// altnrslog can also transfer [slog.Attr].
+//
+// [Logs in Context]: https://docs.newrelic.com/docs/logs/logs-context/logs-in-context/
+package altnrslog
