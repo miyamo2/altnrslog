a1be9d78ecccd9e0aa3bb7c6c8baa2239cde894d
miyamo2 <miyamo2@outlook.com>
2024-02-27 19:11:29 +0900

    added WithInnerHandlerProvider Example

--- exapmles_test.go
+++ exapmles_test.go
@@ -3,6 +3,7 @@ package altnrslog_test
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"log/slog"
 	"net/http"
@@ -12,11 +13,11 @@ import (
 	"github.com/newrelic/go-agent/v3/newrelic"
 )
 
-type ExampleRequest struct {
-	Name string `json:"name"`
-}
-
 func Example() {
+	type Request struct {
+		Name string `json:"name"`
+	}
+
 	nr, err := newrelic.NewApplication(
 		newrelic.ConfigAppName(os.Getenv("NEW_RELIC_CONFIG_APP_NAME")),
 		newrelic.ConfigLicense(os.Getenv("NEW_RELIC_CONFIG_LICENSE")),
@@ -32,7 +33,7 @@ func Example() {
 		logHandler := altnrslog.NewTransactionalHandler(nr, tx)
 		logger := slog.New(logHandler)
 
-		var req ExampleRequest
+		var req Request
 		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
@@ -47,4 +48,35 @@ func Example() {
 	}))
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func ExampleNewTransactionalHandler_WithInnerHandlerProvider() {
+	app, err := newrelic.NewApplication(
+		newrelic.ConfigAppName(os.Getenv("NEW_RELIC_CONFIG_APP_NAME")),
+		newrelic.ConfigLicense(os.Getenv("NEW_RELIC_CONFIG_LICENSE")),
+		newrelic.ConfigAppLogForwardingEnabled(true),
+	)
+	tx := app.StartTransaction("ExampleNewTransactionalHandler_WithInnerHandlerProvider")
+	if err != nil {
+		panic(err)
+	}
+
+	handlerOpt := slog.HandlerOptions{
+		AddSource: true,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			return slog.Attr{
+				Key:   fmt.Sprintf("replaced.%s.%s", groups[0], a.Key),
+				Value: a.Value,
+			}
+		},
+	}
+
+	jsonHandlerProvider := func(w io.Writer) slog.Handler {
+		return slog.NewJSONHandler(w, &handlerOpt)
+	}
+
+	txHandler := altnrslog.NewTransactionalHandler(app, tx,
+		altnrslog.WithInnerHandlerProvider(jsonHandlerProvider))
+
+	slog.New(txHandler)
 }
